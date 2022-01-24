6c72ee9be2ab119e53a2eb66b2ef5b1cf442d659
Lesly <leslyr562@gmail.com>
2022-01-24 03:06:11 -0600

    added authentication to the login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 //InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
 //createHttpLink allows us to control how the Apollo Client makes a request. Think of it like middleware for the outbound network requests.
 //switch is for 404s so  If the route doesn't match any of the preceding paths (e.g., /about), then users will see the 404 message.
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
@@ -24,8 +25,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
